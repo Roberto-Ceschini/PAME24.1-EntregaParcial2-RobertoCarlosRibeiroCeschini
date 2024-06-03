a8f8878850caec0c0df80d9cbb78fe325661db80
Roberto-Ceschini <134228152+Roberto-Ceschini@users.noreply.github.com>
2024-06-02 23:14:33 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 function AlertaLogin() {
     var email = document.getElementById("email").value;
     var senha = document.getElementById("senha").value;
-    var lembrar = document.getElementById("lembrar").checked ? "Sim" : "Não";
+    var lembrar = document.getElementById("chk").checked ? "Sim" : "Não";
 
     alert("Email: " + email + "\nSenha: " + senha + "\nLembrar de mim: " + lembrar);
 }
